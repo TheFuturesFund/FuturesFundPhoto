c04714790d6039aa2deff6b923a1a3a5eebfbd94
Jonathan Hooper <Jonathan@newaperio.com>
2015-08-09 15:40:46 -0500

    Setup foreman
    This commit sets up foreman.
    The foreman file is added to tell foreman which port to operate on.
    The Procfile is added to tell foreman how to start up.
    The values in `.env` can be overriden in `.env.local` which is not tracked.
    
    After this commit, the app should be launched with `foreman start -e .evn,.env.local`
    or with the start script: `./bin/start`.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module FuturesFundPhoto
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Set the applications secret token
+    config.secret_token = ENV['SECRET_KEY_BASE']
   end
 end
