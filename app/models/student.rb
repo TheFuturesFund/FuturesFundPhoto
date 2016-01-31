97dd1a4375deecf72d6848c2ce71c662bb579b38
Jonathan Hooper <jonathan@newaperio.com>
2016-01-30 22:22:41 -0600

    Destroy student's albums when student is destroyed

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,7 @@ class Student < ActiveRecord::Base
   has_one :user, as: :role
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-  has_many :albums
+  has_many :albums, dependent: :destroy
 
   def self.ordered_alphabetically_by_last_name
     order(last_name: :asc)
