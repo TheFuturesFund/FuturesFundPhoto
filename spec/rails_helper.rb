f0a72de36796b59bca26c34006e39acec76addd4
Jonathan Hooper <jonathan@newaperio.com>
2016-09-08 16:21:47 -0500

    Add database cleaner
    This commit adds the database cleaner gem which is used to clean out the database in between running tests.
    This prevents data from previous tests form interfering with running tests
    
    ref: https://github.com/DatabaseCleaner/database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,18 +1,17 @@
-require 'dotenv'
+require "dotenv"
 Dotenv.load
 
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+ENV["RAILS_ENV"] ||= "test"
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
+require File.expand_path("../../config/environment", __FILE__)
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+if Rails.env.production?
+  abort("The Rails environment is running in production mode!")
+end
+
+require "spec_helper"
+require "rspec/rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
-
-RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-end
